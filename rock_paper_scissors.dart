5e27d13fe8ad7c1f24772393db2e28839e27a325
janee85 <melissajanereas241@gmail.com>
2026-09-24 18:02:38 +0800

    "Add validateMove function"

---
+++ rock_paper_scissors.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+void main() {
+  print("===== ROCK, PAPER, SCISSORS =====");
+  stdout.write("Enter player 1 name: "); 
+  String? playerOneName = stdin.readLineSync();
+  String? playerOne = (playerOneName ?? "").trim().toLowerCase();
+  if (playerOne.isEmpty) {
+    playerOne = "Player 1";
+  }
+
+  stdout.write("Enter player 2 name: ");
+  String? playerTwoName = stdin.readLineSync();
+  String? playerTwo = (playerTwoName ?? "").trim().toLowerCase();
+  if (playerTwo.isEmpty) {
+    playerTwo = "Player 2";
+  }
+}
+
+
+String getMove(String playerName) {
+  String? validMove;
+  do {
+    stdout.write("Enter your move $playerName[rock, paper, scissors]: ");
+    String? playerMove = stdin.readLineSync();
+    validMove = validateMove(playerMove);
+    if (validMove == null) {
+      print("Choose rock, paper, scissor only!");
+    }
+  } while (validMove == null);
+
+  return 
+    stdout.write("Enter your move player 1(rock, paper, scissors): ");
+    String? playerOneMove = stdin.readLineSync();
+        for (int i = 0; i <= 30; i++) {
+            print("");
+        }
+    stdout.write("Enter your move player 2(rock, paper, scissors): ");
+    String? playerTwoMove = stdin.readLineSync();
+}
+
+String? validateMove(String? move) {
+  List<String?> validMoves = List.filled(3, null, growable: false);
+  validMoves[0] = "rock";
+  validMoves[1] = "paper";
+  validMoves[2] = "scissor";
+
+  String correctMove = (move ?? "").trim().toLowerCase();
+  if (validMoves.contains(correctMove)) {
+    return correctMove;
+  }
+}
+
+String? decideWinner(String playerOneName, String playerOneMove, String playerTwoName, String playerTwoMove) {
+
+}
